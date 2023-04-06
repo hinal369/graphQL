51a99db0da037dda39017f90be9bfd551381fecc
Hinal <khalasihinal1998@gmail.com>
2023-04-06 12:43:00 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const port = 4000;
+const app = express();
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Listening to the port : ${port}`);
+})
+
