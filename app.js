8729c7be960ddc3876ec577735fc7fbf50bc0aea
Hinal Khalasi <khalasihinal1998@gmail.com>
2023-04-07 11:48:16 +0530

    New-Exist

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const port = 4000;
+const app = express();
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Listening to the port : ${port}`);
+})
